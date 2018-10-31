596e56e5236bdc5313fcccd714a72e3c9dc9e30b
RClarke062 <robertjclarke@sympatico.ca>
2018-10-31 19:57:02 -0400

    Oct 31 Working Image per Swipe

--- CoolPageFragment.java
+++ CoolPageFragment.java
@@ -1,46 +1,19 @@
 package com.robboapps.ottawa4kidstest;
 
 
-import android.app.ListActivity;
-import android.app.ListFragment;
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+//import android.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-//import static android.media.CamcorderProfile.get;
-import static java.lang.String.valueOf;
-
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class CoolPageFragment extends Fragment {
 
     ImageView imageView;
-    //AssetManager assetManager = getContext().getAssets();
-    //Bitmap bitmap;
-    //Integer numImages;
-    //String[] imgList;
-    //should mPosition be private (was in stackoverflow example)
     int mPosition;
-    //ArrayList<Bitmap> picString = new ArrayList<>();
-//empty constructor to make the class available to other classes
-    //public CoolPageFragment(){
-
-    //}
 
     public static CoolPageFragment newInstance(int pos){
         CoolPageFragment frag = new CoolPageFragment();
@@ -61,101 +34,14 @@ public class CoolPageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //thiscontext = container.getContext();
-
-
-        //use getActivity with getApplicationContext to access context based methods such as
-        //getAssets in a non activity class
-
-        //old try block that works
-/*
-        try{
-            AssetManager assetManager = getActivity().getApplicationContext().getAssets();
-            String[] imgList = assetManager.list("trainmuseum");
-            System.out.println("XANADU 9" + Arrays.toString(imgList));
-            numImages = imgList.length;
-            //for(int i = 0; i < imgList.length; i++){
-            //picString.add(String.valueOf(i));
-                //picString.add("trainmuseum/" + imgList[i]);
-            //System.out.print("XANADU15  " + picString.get(i) + " ");
-            //String fred = picString.get(i);
-
-
-
-            System.out.println("XANADU10" + "  " + numImages);
-            String filename1 = "trainmuseum/" + imgList[0];
-            InputStream is1 = assetManager.open(filename1);
-            //InputStream is = assetManager.open("trainmuseum/c.png");
-            //InputStream is = assetManager.open("c");
-
-            bitmap = BitmapFactory.decodeStream(is1);
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-*/
-
-        //new try block to iterate bitmaps into the switch function
-/*
-        try{
-            AssetManager assetManager = getActivity().getApplicationContext().getAssets();
-            imgList = assetManager.list("trainmuseum");
-            System.out.println("XANADU 03" + Arrays.toString(imgList));
-            numImages = imgList.length;
-            for(int i = 0; i < imgList.length; i++){
-            //picString.add(String.valueOf(i));
-                //String filename = "trainmuseum/" + imgList[0];
-                picString.add(i, BitmapFactory.decodeStream(assetManager.open("trainmuseum/" + imgList[i])));
-            //System.out.print("XANADU15  " + picString.get(i) + " ");
-            //String fred = picString.get(i);
-            }
-
-
-            System.out.println("XANADU 04" + "  " + numImages);
-            String filename1 = "trainmuseum/" + imgList[0];
-            InputStream is1 = assetManager.open(filename1);
-            InputStream is = assetManager.open("trainmuseum/c.png");
-            //InputStream is = assetManager.open("c");
-
-            //bitmap = BitmapFactory.decodeStream(is1);
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-
-*/
-
-        //imageView.setImageBitmap(bitmap);
-        //use this if files are in 'drawable' folder
-        //imageView.setImageResource(R.drawable.b);
-        //Bundle bundle = getArguments();
-        //String message = Integer.toString(bundle.getInt("count"));
-        //push check
 
         View view = inflater.inflate(R.layout.fragment_cool_page, container, false);
-        //ImageView coolPhotos = (ImageView) view.findViewById(R.id.imageView); no need to cast
-        // view.findViewById by putting (ImageView) in front of it according to Android Studio
-        // even through the stackoverflow had it
        imageView = view.findViewById(R.id.imageView);
-       imageView.setImageResource(R.drawable.b);
-
-       // switch (mPosition){
-            //case 0:
-                //imageView.setImageBitmap(picString.get(0));
-                //imageView.setImageBitmap(bitmap);
-                //imageView.setImageResource(R.drawable.b);
-                //System.out.print("XANADU 05  ");
-            //case 1:
-                //imageView.setImageBitmap(picString.get(1));
-                //imageView.setImageBitmap(bitmap);
-                //imageView.setImageResource(R.drawable.c);
-                //System.out.print("XANADU 06  ");
-                //default:
-                    //imageView.setImageResource(R.drawable.d);
-                    //System.out.print("XANADU 07  ");
-                //default:
-                    //imageView.setImageBitmap(picString.get(1));
-
-        //}
+       if (mPosition == 1){
+           imageView.setImageResource(R.drawable.b);
+       }else {
+           imageView.setImageResource(R.drawable.c);
+       }
         return view;
     }
 
--- WhatsCoolActivity.java
+++ WhatsCoolActivity.java
@@ -1,29 +1,20 @@
 package com.robboapps.ottawa4kidstest;
 
+
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.AssetManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+//import android.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
-import java.io.IOException;
-
-public class WhatsCoolActivity extends FragmentActivity {
+public class WhatsCoolActivity extends AppCompatActivity {
     ViewPager viewPager;
     CoolSwipeAdaptor swipeAdaptor;
     static final int ITEMS = 2;
-    //static Context context;
-    //context = this.getbaseContext;
-    //Context context = this;
-    //Context context = this;
-    //Context context = Fragment.getContext();
-    //Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,58 +22,20 @@ public class WhatsCoolActivity extends FragmentActivity {
         setContentView(R.layout.activity_whats_cool);
         viewPager = findViewById(R.id.view_pager);
         System.out.println("XANADU First");
-        //CoolSwipeAdaptor swipeAdaptor = new CoolSwipeAdaptor(getSupportFragmentManager(), context);
         swipeAdaptor = new CoolSwipeAdaptor(getSupportFragmentManager());
         viewPager.setAdapter(swipeAdaptor);
-        //viewPager.setCurrentItem(0);
 
-        //Toast.makeText(this, getIntent().getStringExtra("str1"), Toast.LENGTH_SHORT).show();
     }
-// from: https://developer.android.com/training/animation/screen-slide#java
-    //@Override
-    //public void onBackPressed() {
-    //if (viewPager.getCurrentItem() == 0) {
-    //System.out.println("XANADU Second");
-    // If the user is currently looking at the first step, allow the system to handle the
-    // Back button. This calls finish() on this activity and pops the back stack.
-    //super.onBackPressed();
-    //} else {
-    // Otherwise, select the previous step.
-    //System.out.println("XANADU Third");
-    //viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
-    //}
-    //}
 
     public static class CoolSwipeAdaptor extends FragmentStatePagerAdapter {
-        //private Context context2;
 
-        //int numImages;
-        //String[] imgList;
-
-        //testgit23
-        // get Context from WhatsCoolActivity when it creates a new CoolSwipeAdaptor Class, and assign to
-        // context2 as it is required in order to use Asset Manager methods
-        //public CoolSwipeAdaptor(FragmentManager fm, Context context) {
         public CoolSwipeAdaptor(FragmentManager fm) {
             super(fm);
-            //context2 = context;
         }
 
         @Override
         public int getCount() {
-/*
-        try {
 
-                AssetManager assetManager = context2.getApplicationContext().getAssets();
-                imgList = assetManager.list("trainmuseum");
-                System.out.println("XANADU 02" + Arrays.toString(imgList));
-                //numImages = imgList.length;
-
-            }catch (IOException e) {
-                e.printStackTrace();
-            }
-*/
-            //return imgList.length;
             System.out.println("XANADU 01");
             return ITEMS;
 
@@ -91,16 +44,12 @@ public class WhatsCoolActivity extends FragmentActivity {
         @Override
         public Fragment getItem(int pos) {
             System.out.println("XANADU 02  POS IS " + pos);
-            //if(pos < imgList.length);
-            //return CoolPageFragment.newInstance(pos);
 
-            //else
-            //return null;
             switch (pos) {
                 case 0:
-                    return CoolPageFragment.newInstance(0);
+                    return CoolPageFragment.newInstance(pos);
                 case 1:
-                    return CoolPageFragment.newInstance(1);
+                    return CoolPageFragment.newInstance(pos);
                     default:
                         return null;
             }
